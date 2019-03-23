18ded93e108ecd040369c075620a8f993b6f8f01
Admin <admin@oleksiis-mac.home>
2019-03-23 21:31:00 +0100

    Initial commit

---
+++ code.js
@@ -0,0 +1,29 @@
+const mainParagraph = document.querySelector('p.subtitle');
+const txt = "Welcome to my personal website!";
+const spanCursor = document.querySelector('.cursor');
+
+let indexText = 0;
+const time = 40;
+
+const addLetter = () => {
+    mainParagraph.textContent += txt[indexText];
+    indexText++;
+    if (indexText === txt.length) {
+        clearInterval(indexTyping);
+    }
+}
+
+const indexTyping = setInterval(addLetter, time);
+
+const cursorAnimation = () => {
+    spanCursor.classList.toggle('active');
+}
+
+setInterval(cursorAnimation, 400);
+
+const navBar = document.querySelector('i.menu');
+const ul = document.querySelector('ul');
+
+navBar.addEventListener('click', (e) => {
+    ul.classList.toggle('active');
+})
